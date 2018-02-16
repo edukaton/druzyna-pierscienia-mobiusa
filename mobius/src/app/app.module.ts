6bd85f884ea511015ca8cb6b92cf1d6a75329766
Anna Olchowik <ania.olchowik@gmail.com>
2018-02-16 22:04:08 +0100

    routing

--- mobius/src/app/app.module.ts
+++ mobius/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { StartScreenComponent } from './start-screen/start-screen.component';
@@ -16,7 +17,15 @@ import { EndScreenComponent } from './end-screen/end-screen.component';
     EndScreenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path:'start', component:StartScreenComponent},
+      {path:'quiz', component:QuizScreenComponent},
+      {path:'koniec', component:EndScreenComponent},
+      {path:'', redirectTo :'start', pathMatch:'full' },
+      {path:'**', redirectTo :'start', pathMatch:'full' },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
