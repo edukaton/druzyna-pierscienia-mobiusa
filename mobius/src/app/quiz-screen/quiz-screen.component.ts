dc7756c9f6024aa00e525cff45d47a840cc0b14c
Piotr Migdał <pmigdal@gmail.com>
2018-02-17 10:53:01 +0100

    question id misc

--- mobius/src/app/quiz-screen/quiz-screen.component.ts
+++ mobius/src/app/quiz-screen/quiz-screen.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
+
 @Component({
   selector: 'app-quiz-screen',
   templateUrl: './quiz-screen.component.html',
   styleUrls: ['./quiz-screen.component.css']
 })
 export class QuizScreenComponent implements OnInit {
-  
-  currentQuestion:number=0;
 
-  changeQuestion(increment:number){
-    this.currentQuestion+=increment;
+  currentQuestionId: number = 0;
+
+  changeQuestion(increment: number) {
+    this.currentQuestionId += increment;
   }
-  
+
   constructor() { }
 
   ngOnInit() {
