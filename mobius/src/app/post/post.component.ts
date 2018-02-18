7eb77f18e989c094a0785e10cee3d563c974b9b9
Anna Olchowik <ania.olchowik@gmail.com>
2018-02-18 12:09:39 +0100

    messenger style:)

--- mobius/src/app/post/post.component.ts
+++ mobius/src/app/post/post.component.ts
@@ -11,9 +11,20 @@ export class PostComponent implements OnInit {
   @Input() picture: string;
   @Input() links: string[];
   @Input() postType: string;
-  constructor() { }
+  postTypeSelector:any = {
+    "question":"bg-primary text-white",
+    "otherPerson":"bg-primary text-white",
+    "answer":"bg-light",
+    "conclusion":"bg-primary text-white",
+    "Ty":"bg-warning text-white"}
+  constructor() { 
+  }
 
   ngOnInit() {
+    //console.log("this.person : "+this.person)
+    if(this.person=="Ty") {
+      this.postType = "Ty";
+    }
   }
 
 }
